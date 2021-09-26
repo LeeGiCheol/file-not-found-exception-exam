8d0b9dc5a4d49f59cca09bfdd95a4fbef8095228
LeeGiCheol <asdqwek200@naver.com>
2021-09-26 21:51:35 +0900

    Jar Package 시 File 객체 에러

---
+++ src/main/java/com/example/fileexam/Runner.java
@@ -0,0 +1,86 @@
+package com.example.fileexam;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class Runner implements ApplicationRunner {
+
+    private final String NUMBER_PATH = "number.txt";
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        fileCallSuccess();
+        fileCallFail();
+        fileCallToFile();
+    }
+
+    public void fileCallSuccess() throws IOException {
+        Resource resource = new ClassPathResource(this.NUMBER_PATH);
+        InputStream is = resource.getInputStream();
+        Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(reader);
+
+        String line;
+        int sum = 0;
+
+        while ((line = br.readLine()) != null) {
+            sum += Integer.parseInt(line);
+        }
+
+        System.out.println(sum);
+    }
+
+    public void fileCallFail() throws IOException {
+        Resource resource = new ClassPathResource(this.NUMBER_PATH);
+        String path = resource.getFile().getAbsolutePath();
+
+        FileInputStream fis = new FileInputStream(path);
+
+        Reader reader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(reader);
+
+        String line;
+        int sum = 0;
+
+        while ((line = br.readLine()) != null) {
+            sum += Integer.parseInt(line);
+        }
+
+        System.out.println(sum);
+    }
+
+    public void fileCallToFile() throws IOException {
+        Resource resource = new ClassPathResource(this.NUMBER_PATH);
+        InputStream is = resource.getInputStream();
+
+        File numberFile = File.createTempFile("number", ".txt");
+        try (FileOutputStream fos = new FileOutputStream(numberFile)) {
+
+            int read;
+            byte[] bytes = new byte[1024];
+
+            while ((read = is.read(bytes)) != -1) {
+                fos.write(bytes, 0, read);
+            }
+        }
+
+        fileRead(numberFile);
+    }
+
+    private void fileRead(File numberFile) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(numberFile));
+            String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+}
